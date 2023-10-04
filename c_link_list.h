51b23d7a51b43ce83d392b81f3b8085e6cf3d2ba
morningcat <morningcat2018@qq.com>
2023-10-04 17:21:08 +0800

    c语言实现单链表

---
+++ c_link_list.h
@@ -0,0 +1,150 @@
+#ifndef C_LINK_LIST_H
+#define C_LINK_LIST_H
+
+// #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef char ElemType;
+
+typedef struct node
+{
+    ElemType data;
+    struct node *next;
+} ListNode;
+
+// 初始化链表
+ListNode *newList()
+{
+    ListNode *head = (ListNode *)malloc(sizeof(ListNode));
+    head->next = NULL;
+    return head;
+}
+
+// 销毁链表
+void destoryList(ListNode *head)
+{
+    ListNode *p, *q;
+    p = head;
+    q = p->next;
+    while (q != NULL)
+    {
+        free(p);
+        p = q;
+        q = p->next;
+    }
+    free(p);
+}
+
+// 判空
+int isEmpty(ListNode *head)
+{
+    return head->next == NULL;
+}
+
+// 长度
+int getLength(ListNode *head)
+{
+    ListNode *p = head;
+    int length = 0;
+    while (p->next != NULL)
+    {
+        length++;
+        p = p->next;
+    }
+    return length;
+}
+
+void printList(ListNode *head)
+{
+    ListNode *p;
+    for (p = head->next; p != NULL; p = p->next)
+        printf("%c ", p->data);
+    printf("\n");
+}
+
+ElemType valueOf(ListNode *head, int index)
+{
+    ListNode *p;
+    int j = 0;
+    for (p = head->next; p != NULL; p = p->next)
+    {
+        j++;
+        if (j == index)
+            return p->data;
+    }
+    return 0;
+}
+
+int indexOf(ListNode *head, ElemType value)
+{
+    ListNode *p;
+    int flag = 0, i = 0;
+    for (p = head->next; p != NULL; p = p->next)
+    {
+        i++;
+        if (p->data == value)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int insertNode(ListNode *head, int index, ElemType value)
+{
+    assert(head != NULL);
+    ListNode *p, *t;
+    int j = 0;
+    for (p = head; p != NULL; p = p->next)
+    {
+        j++;
+        if (j == index)
+        {
+            t = (ListNode *)malloc(sizeof(ListNode));
+            t->data = value;
+            t->next = p->next;
+            p->next = t;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void insert(ListNode *head, ElemType value)
+{
+    assert(head != NULL);
+    ListNode *p, *t;
+    for (p = head; p != NULL; p = p->next)
+    {
+        if (p->next == NULL)
+        {
+            t = (ListNode *)malloc(sizeof(ListNode));
+            t->data = value;
+            t->next = NULL;
+            p->next = t;
+            return;
+        }
+    }
+}
+
+int deleteNode(ListNode *head, int index)
+{
+    assert(head != NULL);
+    ListNode *p = head, *q;
+    int j = 0;
+    for (p = head; p != NULL; p = p->next)
+    {
+        j++;
+        if (j == index)
+        {
+            q = p->next;
+            p->next = p->next->next;
+            free(q);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+#endif // C_LINK_LIST_H
---
+++ c_link_list_test.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "c_link_list.h"
+
+// gcc c_link_list_test.c && ./a.out
+int main()
+{
+    printf("（1）初始化单链表h\n");
+    ListNode *head = newList();
+    printf("（2）依次插入a,b,c,d,e元素\n");
+    insert(head, 'a');
+    insert(head, 'b');
+    insert(head, 'c');
+    insert(head, 'd');
+    insert(head, 'e');
+    printf("（3）输出单链表:");
+    printList(head);
+    printf("（4）获取链表长度:");
+    printf("%d\n", getLength(head));
+    printf("（5）是否为空:");
+    printf("%d\n", isEmpty(head));
+    printf("（6）查询特点位置的值");
+    printf("%c\n", valueOf(head, 3));
+    printf("（7）查询位置");
+    printf("索引位置%d\n", indexOf(head, 'a'));
+    printf("（8）插入节点\n");
+    insertNode(head, 4, 'f');
+    printf("（9）输出单链表:");
+    printList(head);
+    printf("（10）删除节点\n");
+    deleteNode(head, 3);
+    printf("（11）输出单链表:");
+    printList(head);
+    printf("（12）释放单链表\n");
+    destoryList(head);
+    return 0;
+}
